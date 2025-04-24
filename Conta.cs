6d0143d2df304cb5ac49626b629275538975b795
Thiago Braz <thiagoadsbraz@gmail.com>
2025-04-23 20:25:16 -0400

    Inicio do Projeto

---
+++ Conta.cs
@@ -0,0 +1,37 @@
+﻿
+public class Conta
+{
+    public int id;
+    public string agencia;
+    public string Nconta;
+    public string nomePro;
+    public double saldo;
+
+    public void Depositar(double valor)
+    {
+        if (valor >= 0)
+        {
+            saldo += valor;
+            Console.WriteLine($"Depósito de {valor} realizado com sucesso. Saldo atual: {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Valor de depósito inválido.");
+        }
+    }
+    public void Sacar(double valor)
+    {
+        if (valor > 0 && valor <= saldo)
+        {
+            saldo -= valor;
+            Console.WriteLine($"Saque de {valor} realizado com sucesso. Saldo atual: {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Valor de saque inválido ou saldo insuficiente.");
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+/*Conta c1 = new Conta();
+
+Console.WriteLine("Digite o valor ID");
+c1.id = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a agencia");
+c1.agencia = Console.ReadLine();
+
+Console.WriteLine("Digite o numero da conta");
+c1.Nconta = Console.ReadLine();
+
+Console.WriteLine("Digite o nome do proprietario");
+c1.nomePro = Console.ReadLine();
+
+Console.WriteLine("\n");
+
+c1.Depositar(100);
+c1.Sacar(50);*/
+
+List<Conta> contas = new List<Conta>();
+
+while (true)
+{
+    Console.WriteLine("Digite 1 para cadastrar nova conta");
+    Console.WriteLine("Digite 2 para listar todas");
+    int opc = Convert.ToInt32(Console.ReadLine());
+
+    if (opc == 1)
+    {
+        Console.WriteLine("Digite o valor ID");
+        int id = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite a agencia");
+        string agencia = Console.ReadLine();
+        Console.WriteLine("Digite o numero da conta");
+        string Nconta = Console.ReadLine();
+        Console.WriteLine("Digite o nome do proprietario");
+        string nomePro = Console.ReadLine();
+        Conta c1 = new Conta();
+        c1.id = id;
+        c1.agencia = agencia;
+        c1.Nconta = Nconta;
+        c1.nomePro = nomePro;
+        contas.Add(c1);
+    }
+    else if (opc == 2)
+    {
+        foreach (var item in contas)
+        {
+            Console.WriteLine($"ID: {item.id}, Agencia: {item.agencia}, Conta: {item.Nconta}, Nome: {item.nomePro}");
+        }
+
+    }
+}
